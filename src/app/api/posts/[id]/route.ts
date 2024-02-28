1283fa4c8079e4d4d6dabbf37dace75779b2c660
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-02-28 23:18:16 +0200

    ADD: googleAuth

--- src/app/api/posts/[id]/route.ts
+++ src/app/api/posts/[id]/route.ts
@@ -4,17 +4,10 @@ import { redirect } from 'next/navigation'
 
 export async function DELETE(
     req: Request,
+
     { params }: { params: { id: string } },
 ) {
     const { id } = params
 
-    const headersList = headers()
-    const type = headersList.get('Content-type')
-
-    const cookiesList = cookies()
-    const coo1 = cookiesList.get('Cookie_1')?.value
-
-    // redirect('/blog')
-
-    return NextResponse.json({ coo1 })
+    return NextResponse.json({ id })
 }

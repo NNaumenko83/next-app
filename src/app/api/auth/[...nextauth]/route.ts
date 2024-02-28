54e9558a956e71fe06e132776144e63f9d89c1ba
Mykola <108565397+NNaumenko83@users.noreply.github.com>
2024-02-29 00:10:17 +0200

    UPD: tsconfig

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,6 @@
+authConfig
+import { authConfig } from '@/configs/auth'
 import NextAuth from 'next-auth'
-import { authConfig } from '../../../../../configs/auth'
 
 const handler = NextAuth(authConfig)
 
